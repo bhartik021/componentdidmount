6c7e1a4ed024d6a0e1afdc1769b51ef54afbddf7
bhartik021 <bhartik021@gmail.com>
2022-06-30 19:51:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    //Initializing the state
+
+    this.state = { color: "Red" };
+  }
+
+  componentDidMount() {
+    //changing the state after 2 sec from the time when the component is rendered
+
+    setTimeout(() => {
+      this.setState({ color: "white" });
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <div>
+        <p
+          style={{
+            color: this.state.color,
+            backgroundColor: "#060c21",
+            textAlign: "center",
+            fontSize: "200px",
+            width: "100%",
+            height: "100vh",
+            margin: "auto",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Bharti Kumari
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
